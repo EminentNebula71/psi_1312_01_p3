b9db4b971c97183755f716bd6c56186ef023f294
Angel <tyxy@hotmail.com>
2020-12-14 21:07:32 +0100

    css empezado

--- labassign/labassign/settings.py
+++ labassign/labassign/settings.py
@@ -16,6 +16,13 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASE_URL = 'postgres://alumnodb:alumnodb@localhost:5432/psi'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
+
+STATIC_URL = '/static/' # You may find this is already defined as such.
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
 
 # Log In Log Out process
 AUTH_USER_MODEL = 'core.Student'
@@ -126,8 +133,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 #AUTH_USER_MODEL = 'core.Student'
 
 # Parse database configuration from $DATABASE_URL
@@ -135,4 +140,4 @@ STATIC_URL = '/static/'
 import dj_database_url
 DATABASES['default']= dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/psi')
 """
-STATIC_ROOT = 'staticfiles'
+#STATIC_ROOT = 'staticfiles'
